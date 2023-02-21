6373922b5e205d938c27da56d7cd4647223292f6
enmanuel <enmanuelbta14@gmail.com>
2023-02-21 14:23:53 -0500

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let multiply;
+const num1 = 2;
+const num2 = 31;
+multiply = num1 * num2;
+let random;
+random = Math.floor(Math.random() * 10) + 1;
+let mod;
+const num3 = 4;
+const num4 = 8;
+mod = num3 % num4;
+let max;
+max = Math.max(10,20);
 
-// Write your code here
+
+
+// Write your code heren
